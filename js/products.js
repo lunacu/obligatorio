58ab8222ffb6c215c230abaaf2dab75be0521190
Jorge Deurado <lldeurado@outlook.es>
2020-08-15 02:46:37 -0300

    Listado de productos mostrado y login hecho falta centrar la lista de productos y hacer diagrama

--- js/products.js
+++ js/products.js
@@ -1,6 +1,47 @@
+var productsArray = [];
+
+
+
+
+function showProductsList(array) {
+
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div>` + product.cost + `</div> <br>
+                        <div>` + product.name + `</div> <br>
+                        <div>` + product.description + `</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+
+
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e) {
+    showSpinner();
+    getJSONData(PRODUCTS_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            hideSpinner();
+            productsArray = resultObj.data;
+            //Muestro los productos
+            showProductsList(productsArray);
+        }
+    });
 });
